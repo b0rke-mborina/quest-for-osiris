0b6e78b235c87f60f0c6b7e635df9c35be415f3c
Danijel Dupor <danijeldupor.dd@gmail.com>
2023-05-07 17:37:02 +0200

    Dodani leveli na glavni izbornik

--- Assets/Scripts/LevelsMenu.cs
+++ Assets/Scripts/LevelsMenu.cs
@@ -6,9 +6,17 @@ using UnityEngine.SceneManagement;
 public class LevelsMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void LevelSelect()
+    public void Level1Select()
     {
         SceneManager.LoadScene("Level1");
+    }
+    public void Level3Select()
+    {
+        SceneManager.LoadScene("Level3Scene");
+    }
+    public void Level4Select()
+    {
+        SceneManager.LoadScene("Level4Scene");
     }
     public void Back()
     {
