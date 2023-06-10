aa2d8bf2a2f69dcbb63b5c4a0f538e0aa3300d13
b0rke-mborina <mborina@net.hr>
2023-06-10 19:41:27 +0200

    Implemented changeable platforms
    - platforms that can be changed from opaque to transparent
    - every other platform is transparent
    - magic is used to manipulate the platforms

---
+++ Assets/Scripts/AlphaChange.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaChange : MonoBehaviour
+{
+    private Renderer model;
+
+    void Start()
+    {
+        model = GetComponent<Renderer>();
+    }
+
+    public void MakeTransparent()
+    {
+        Color color = model.material.color;
+        color.a = 0.5f;
+        model.material.color = color;
+    }
+
+    public void MakeOpaque()
+    {
+        Color color = model.material.color;
+        color.a = 1;
+        model.material.color = color;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(model.material);
+    }
+}
