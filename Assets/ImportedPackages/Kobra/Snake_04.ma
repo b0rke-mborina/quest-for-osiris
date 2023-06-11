d98487da56945f5107a28c57bdd435f78ef5e221
Danijel Dupor <danijeldupor.dd@gmail.com>
2023-06-11 20:37:22 +0200

    enemy

---
+++ Assets/ImportedPackages/Kobra/Snake_04.ma
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9f32fc2ba7674b5f0c7e88a888fb07b86b33431bdbf70f74a73c14992f729f2
+size 145995
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float MaxSpeed;
+    private float Speed;
+
+    private Collider[] hitColliders;
+    private RaycastHit Hit;
+
+    public float SightRange;
+    public float DetectionRange;
+    public Rigidbody rb;
+    public GameObject Target;
+
+    private bool seePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed= MaxSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!seePlayer) {
+            hitColliders= Physics.OverlapSphere(transform.position, DetectionRange);
+            foreach (var HitCollider in hitColliders)
+            {
+                if(HitCollider.tag=="Player") {
+                    Target= HitCollider.gameObject;
+                    seePlayer= true;
+                }
+            }
+        }
+        else {
+            if(Physics.Raycast(transform.position, (Target.transform.position - transform.position), out Hit, SightRange)) {
+                if(Hit.collider.tag== "Player") {
+                    seePlayer= false;
+                }
+                else {
+                    var Heading= Hit.transform.position - transform.position;
+                    var Distance= Heading.magnitude;
+                    var Direction= Heading / Distance;
+
+
+                    Vector3 Move= new Vector3(Direction.x * Speed, 0, Direction.z * Speed);
+                    rb.velocity= Move;
+                    transform.forward= Move;
+                }
+            }
+        }
+    }
+}
