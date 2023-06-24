52d12832ca72cffe6e3328158ed6727ffdbce14c
b0rke-mborina <mborina@net.hr>
2023-06-24 17:00:39 +0200

    Level 3 Puzzle 2
    - finished implementing puzzle 2 for level 3
    - laser system with 4 lasers
    - one laser is already connected, one mark is not activated, one laser is blocked by a pyramid and the last laser is rotated

--- Assets/Scripts/Level3Puzzle2/Level3Puzzle2Handler.cs
+++ Assets/Scripts/Level3Puzzle2/Level3Puzzle2Handler.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Level3Puzzle2Handler : MonoBehaviour
 {
+    GameObject puzzleTask;
+    GameObject puzzle;
+    GameObject leftDoor;
+    GameObject rightDoor;
+
     GameObject laserTurret1;
     GameObject laserTurret2;
     GameObject laserTurret3;
@@ -26,6 +32,11 @@ public class Level3Puzzle2Handler : MonoBehaviour
     Quaternion laserTurret1OriginalRotation;
     Quaternion laserTurret1RotatedRotation;
 
+    LineRenderer laserTurret1CannonLineRenderer;
+    LineRenderer laserTurret2CannonLineRenderer;
+    LineRenderer laserTurret3CannonLineRenderer;
+    LineRenderer laserTurret4CannonLineRenderer;
+
     Vector3 laserMark1ActivatedPosition;
     Vector3 laserMark1DeactivatedPosition;
     Vector3 laserMark2ActivatedPosition;
@@ -45,20 +56,34 @@ public class Level3Puzzle2Handler : MonoBehaviour
 
     string labelTextPress = "[LMB] Press";
     string labelTextRotate = "[LMB] Rotate";
-    string labelTextUseMagic = "[LMB] Use magic";
+    string labelTextUseMagic = "[F] Use magic";
 
-    bool canInteractWithPuzzleElements;
-    bool isCompleted;
+    bool isInRangeOfPuzzleElements;
+    bool isPyramidMoved;
+    bool isLaserMark1Activated;
+    bool isLaserMark2Activated;
+    bool isLaserMark3Activated;
+    bool isLaserMark4Activated;
+    bool isMarkAcitvationEnabled;
+    bool isLaserTurret1Rotated;
+    bool isRotationEnabled;
+    bool isLaserSystemCompleted;
+    bool doorIsOpen;
 
     // Start is called before the first frame update
     void Start()
     {
+        puzzleTask = this.gameObject;
+        puzzle = puzzleTask.transform.parent.gameObject;
+        leftDoor = puzzle.transform.GetChild(2).GetChild(0).gameObject;
+        rightDoor = puzzle.transform.GetChild(2).GetChild(1).gameObject;
+
         laserTurret1 = transform.Find("LaserTurret1").gameObject;
         laserTurret2 = transform.Find("LaserTurret2").gameObject;
         laserTurret3 = transform.Find("LaserTurret3").gameObject;
         laserTurret4 = transform.Find("LaserTurret4").gameObject;
 
-        laserTurret1Canon = transform.Find("LaserTurret1/Canon").gameObject;
+        laserTurret1Canon = transform.Find("LaserTurret1/Canon1").gameObject;
         laserTurret2Canon = transform.Find("LaserTurret2/Canon").gameObject;
         laserTurret3Canon = transform.Find("LaserTurret3/Canon").gameObject;
         laserTurret4Canon = transform.Find("LaserTurret4/Canon").gameObject;
@@ -73,47 +98,245 @@ public class Level3Puzzle2Handler : MonoBehaviour
         laserTurret1OriginalRotation = laserTurret1.transform.localRotation;
         laserTurret1RotatedRotation = Quaternion.Euler(0f, 0f, -90f);
 
+        laserTurret1CannonLineRenderer = laserTurret1Canon.GetComponent<LineRenderer>();
+        laserTurret1CannonLineRenderer.SetPosition(0, laserTurret1Canon.transform.position);
+        laserTurret1CannonLineRenderer.SetPosition(1, laserTurret1Canon.transform.position + Vector3.right * 22.0f);
+
+        laserTurret2CannonLineRenderer = laserTurret2Canon.GetComponent<LineRenderer>();
+        laserTurret2CannonLineRenderer.SetPosition(0, laserTurret2Canon.transform.position);
+        laserTurret2CannonLineRenderer.SetPosition(1, laserTurret2Canon.transform.position + Vector3.left * 10.0f);
+
+        laserTurret3CannonLineRenderer = laserTurret3Canon.GetComponent<LineRenderer>();
+        laserTurret3CannonLineRenderer.SetPosition(0, laserTurret3Canon.transform.position);
+        laserTurret3CannonLineRenderer.SetPosition(1, laserTurret3Canon.transform.position + Vector3.left * 22.0f);
+
+        laserTurret4CannonLineRenderer = laserTurret4Canon.GetComponent<LineRenderer>();
+        laserTurret4CannonLineRenderer.SetPosition(0, laserTurret4Canon.transform.position);
+        laserTurret4CannonLineRenderer.SetPosition(1, laserTurret4Canon.transform.position + Vector3.left * 15.0f);
+
         laserMark1ActivatedPosition = laserMark1.transform.localPosition;
         laserMark1DeactivatedPosition = new Vector3(0f, 1.780536f, -0.43f);
         laserMark2ActivatedPosition = laserMark2.transform.localPosition;
         laserMark2DeactivatedPosition = new Vector3(0f, 1.780536f, -0.43f);
         laserMark3ActivatedPosition = laserMark3.transform.localPosition;
         laserMark3DeactivatedPosition = new Vector3(0f, 1.780536f, -0.43f);
-        laserMark4ActivatedPosition = new Vector3(0f, 1.780536f, -0.43f);
+        laserMark4ActivatedPosition = new Vector3(0f, 1.780536f, -0.93f);
         laserMark4DeactivatedPosition = laserMark4.transform.localPosition;
 
         pyramidOriginalPosition = pyramid.transform.localPosition;
-        pyramidMovedPosition = new Vector3(-10.928f, -12.65f, 1.3197f); ;
+        pyramidMovedPosition = new Vector3(-10.928f, -12.65f, 1.3197f);
 
         interactPromptCanvas = GameObject.Find("Canvas");
         interactPromptText = interactPromptCanvas.GetComponentInChildren<Text>();
         mainCamera = GameObject.Find("Main Camera");
 
-        canInteractWithPuzzleElements = false;
-        isCompleted = false;
+        isInRangeOfPuzzleElements = false;
+        isPyramidMoved = false;
+        isLaserMark1Activated = true;
+        isLaserMark2Activated = true;
+        isLaserMark3Activated = true;
+        isLaserMark4Activated = false;
+        isMarkAcitvationEnabled = true;
+        isLaserTurret1Rotated = false;
+        isRotationEnabled = true;
+        isLaserSystemCompleted = false;
+        doorIsOpen = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        canInteractWithPuzzleElements = true;
+        isInRangeOfPuzzleElements = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        canInteractWithPuzzleElements = false;
+        isInRangeOfPuzzleElements = false;
         interactPromptText.text = "";
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Player" && isInRangeOfPuzzleElements && !doorIsOpen && !isLaserSystemCompleted)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, Mathf.Infinity))
+            {
+                switch (hit.collider.gameObject.name)
+                {
+                    case "LaserMark1":
+                        interactPromptText.text = labelTextPress;
+                        if (Input.GetKey(KeyCode.Mouse0) && isMarkAcitvationEnabled)
+                        {
+                            ChangeMarkActivation(laserMark1, 1);
+                        }
+                        break;
+                    case "LaserMark2":
+                        interactPromptText.text = labelTextPress;
+                        if (Input.GetKey(KeyCode.Mouse0) && isMarkAcitvationEnabled)
+                        {
+                            ChangeMarkActivation(laserMark2, 2);
+                        }
+                        break;
+                    case "LaserMark3":
+                        interactPromptText.text = labelTextPress;
+                        if (Input.GetKey(KeyCode.Mouse0) && isMarkAcitvationEnabled)
+                        {
+                            ChangeMarkActivation(laserMark3, 3);
+                        }
+                        break;
+                    case "LaserMark4":
+                        interactPromptText.text = labelTextPress;
+                        if (Input.GetKey(KeyCode.Mouse0) && isMarkAcitvationEnabled)
+                        {
+                            ChangeMarkActivation(laserMark4, 4);
+                        }
+                        break;
+                    case "PyramidBase":
+                        goto case "PyramidTip";
+                    case "PyramidTip":
+                        interactPromptText.text = labelTextUseMagic;
+                        if (Input.GetKey(KeyCode.F))
+                        {
+                            MovePyramid();
+                        }
+                        break;
+                    case "BodyBottom1":
+                        goto case "Cannon1";
+                    case "BodyMiddle1":
+                        goto case "Cannon1";
+                    case "Head1":
+                        goto case "Cannon1";
+                    case "Cannon1":
+                        interactPromptText.text = labelTextRotate;
+                        if (Input.GetKey(KeyCode.Mouse0) && isRotationEnabled)
+                        {
+                            RotateLaserTurret1();
+                        }
+                        break;
+                    default:
+                        interactPromptText.text = "";
+                        break;
+                }
+            }
+            else
+            {
+                interactPromptText.text = "";
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // RaycastHit hit;
-        // Physics.Raycast(laserTurret3Canon.transform.position, laserTurret3Canon.transform.right, out hit, Mathf.Infinity);
+        if (isLaserMark1Activated && isLaserMark2Activated && isLaserMark3Activated && isLaserMark4Activated && isLaserTurret1Rotated && isPyramidMoved && !doorIsOpen && !isLaserSystemCompleted)
+        {
+            isLaserSystemCompleted = true;
+            OpenDoor();
+        }
+    }
 
-        Ray ray = new Ray(laserTurret3Canon.transform.position, laserTurret3Canon.transform.up);
-        Debug.DrawRay(ray.origin, ray.direction * 10);
+    private void EnableMarkActivation()
+    {
+        isMarkAcitvationEnabled = true;
+    }
 
-        // Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
-        // Debug.DrawRay(transform.position, forward, Color.green);
+    private void ChangeMarkActivation(GameObject mark, int indexOfMark)
+    {
+        switch (indexOfMark)
+        {
+            case 1:
+                if (mark.transform.localPosition.Equals(laserMark1ActivatedPosition))
+                {
+                    mark.transform.localPosition = laserMark1DeactivatedPosition;
+                    isLaserMark1Activated = false;
+                }
+                else
+                {
+                    mark.transform.localPosition = laserMark1ActivatedPosition;
+                    isLaserMark1Activated = true;
+                }
+                break;
+            case 2:
+                if (mark.transform.localPosition.Equals(laserMark2ActivatedPosition))
+                {
+                    mark.transform.localPosition = laserMark2DeactivatedPosition;
+                    isLaserMark2Activated = false;
+                }
+                else
+                {
+                    mark.transform.localPosition = laserMark2ActivatedPosition;
+                    isLaserMark2Activated = true;
+                }
+                break;
+            case 3:
+                if (mark.transform.localPosition.Equals(laserMark3ActivatedPosition))
+                {
+                    mark.transform.localPosition = laserMark3DeactivatedPosition;
+                    isLaserMark3Activated = false;
+                }
+                else
+                {
+                    mark.transform.localPosition = laserMark3ActivatedPosition;
+                    isLaserMark3Activated = true;
+                }
+                break;
+            case 4:
+                if (mark.transform.localPosition.Equals(laserMark4ActivatedPosition))
+                {
+                    mark.transform.localPosition = laserMark4DeactivatedPosition;
+                    isLaserMark4Activated = false;
+                }
+                else
+                {
+                    mark.transform.localPosition = laserMark4ActivatedPosition;
+                    isLaserMark4Activated = true;
+                }
+                break;
+        }
+
+        isMarkAcitvationEnabled = false;
+        Invoke("EnableMarkActivation", 0.5f);
+    }
+
+    private void MovePyramid()
+    {
+        pyramid.transform.localPosition = pyramidMovedPosition;
+        laserTurret2CannonLineRenderer.SetPosition(1, laserTurret2Canon.transform.position + Vector3.left * 15.0f);
+        isPyramidMoved = true;
+    }
+
+    private void EnableTurretRotation()
+    {
+        isRotationEnabled = true;
+    }
+
+    private void RotateLaserTurret1()
+    {
+        if (laserTurret1.transform.localRotation.Equals(laserTurret1OriginalRotation))
+        {
+            laserTurret1.transform.localRotation = laserTurret1RotatedRotation;
+            laserTurret1CannonLineRenderer.SetPosition(0, laserTurret1Canon.transform.position);
+            laserTurret1CannonLineRenderer.SetPosition(1, laserTurret1Canon.transform.position + Vector3.left * 22.0f);
+            isLaserTurret1Rotated = true;
+        }
+        else
+        {
+            laserTurret1.transform.localRotation = laserTurret1OriginalRotation;
+            laserTurret1CannonLineRenderer.SetPosition(0, laserTurret1Canon.transform.position);
+            laserTurret1CannonLineRenderer.SetPosition(1, laserTurret1Canon.transform.position + Vector3.right * 22.0f);
+            isLaserTurret1Rotated = false;
+        }
+        isRotationEnabled = false;
+        Invoke("EnableTurretRotation", 0.5f);
+    }
+
+    private void OpenDoor()
+    {
+        if (!doorIsOpen)
+        {
+            leftDoor.transform.localPosition += Vector3.left * 12.24f;
+            rightDoor.transform.localPosition += Vector3.right * 12.24f;
+            doorIsOpen = true;
+        }
     }
 }
